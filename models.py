7cb9fb04c6d192dcec169fe0744d6a6d28c4882c
namithatholasi <ntholasi@gmail.com>
2025-09-29 10:53:36 -0400

    Update models.py

--- models.py
+++ models.py
@@ -9,7 +9,9 @@ class SurveySubmission(BaseModel):
     consent: bool = Field(..., description="Must be true to accept")
     rating: int = Field(..., ge=1, le=5)
     comments: Optional[str] = Field(None, max_length=1000)
-  
+    source: Literal["homepage","email","qr","other"] = "other"
+    user_agent: Optional[str] = None
+    submission_id: Optional[str] = None
 
     @validator("comments")
     def _strip_comments(cls, v):
@@ -22,6 +24,19 @@ class SurveySubmission(BaseModel):
         return v
         
 #Good example of inheritance
-class StoredSurveyRecord(SurveySubmission):
+class StoredSurveyRecord(BaseModel):
+    name: str
+    consent: bool
+    rating: int
+    comments: Optional[str] = None
+    source: Literal["homepage","email","qr","other"] = "other"
+    user_agent: Optional[str] = None
+
+    hashed_email: str
+    hashed_age: str
+    submission_id: str
+
     received_at: datetime
     ip: str
+    
+    
